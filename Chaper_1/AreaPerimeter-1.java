f9b70050593613dc4bda69ad28fd37cff476bb6f
ZacB2003 <59612875+ZacB2003@users.noreply.github.com>
2020-01-21 07:09:26 -0700

    Add files via upload
    Added Chapter 1 files.

---
+++ Chaper_1/AreaPerimeter-1.java
@@ -0,0 +1,12 @@
+/*
+Author: Zac Burrows
+Date: 1/16/2020
+
+This program is used to show how Java can solve mathematical equations involving area and perimeter of a shape.
+*/
+class AreaPerimeter {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println((2 * 4.5) + (2 * 7.9));
+	}
+}
---
+++ Chaper_1/Cramer'sRule(2).java
@@ -0,0 +1,14 @@
+/*
+Author: Zac Burrows
+Date: 1/16/2020
+
+This program solves a math equation utilizing Cramer's Rule.
+*/	
+class CramersRule {
+	public static void main(String[] args) {
+	System.out.println("x = ");
+	System.out.println(((44.5*0.55)-(50.2*5.9))/((3.4*0.55)-(50.2*2.1)));
+	System.out.println("y = ");
+	System.out.println(((3.4*5.9)-(44.5*2.1))/((3.4*0.55)-(50.2*2.1)));
+	}
+}
